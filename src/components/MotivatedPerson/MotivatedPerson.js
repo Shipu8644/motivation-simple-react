9f6e34e5e56dbb7f3c404876b004a4c097caaeed
Shipu8644 <shipu.monjurur@gmail.com>
2021-09-26 10:35:19 +0600

    icon & star done

--- src/components/MotivatedPerson/MotivatedPerson.js
+++ src/components/MotivatedPerson/MotivatedPerson.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCheck } from '@fortawesome/free-solid-svg-icons';
+import Rating from 'react-rating';
 import './MotivatedPerson.css'
 const MotivatedPerson = (props) => {
     // console.log(props);
-    const { img, name, gender, earning, followers, age } = props.person;
+    const { img, name, gender, earning, followers, age, star } = props.person;
+    const element = <FontAwesomeIcon icon={faUserCheck} />
     return (
         <div className='person'>
             <img className='circular-img' src={img} alt="" />
@@ -11,8 +15,14 @@ const MotivatedPerson = (props) => {
             <p>Gender: {gender}</p>
             <p>Earning: <strong style={{ color: 'aquamarine' }}>${earning}</strong></p>
             <p>Followers: {followers}</p>
+            <Rating className='Rating' initialRating={star}
+                emptySymbol="far fa-star "
+                fullSymbol="fas fa-star "
+                readonly
+            />
+            <br /><br />
             <button onClick={() => props.addToCartHandle(props.person)}
-                className='btn-regular'>Add to Cart</button>
+                className='btn-regular'>{element} Add to Cart</button>
 
         </div>
     );
--- src/components/PersonCart/PersonCart.js
+++ src/components/PersonCart/PersonCart.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 import PersonName from '../PersonName/PersonName';
 import './PersonCart.css'
 const PersonCart = (props) => {
+    const element = <FontAwesomeIcon icon={faUser} />
     console.log(props);
     const { personCart } = props;
     const total = personCart.reduce((a, b) => a + b.earning, 0);
 
     return (
         <div className="personCart">
-            <h2 ><i style={{ color: 'aquamarine' }}>Persons Added: <span style={{ color: 'red' }}> {personCart.length}</span></i></h2>
+            <h2 ><i style={{ color: 'aquamarine' }}>{element} Persons Added: <span style={{ color: 'red' }}> {personCart.length}</span></i></h2>
             <h3 style={{ color: 'aquamarine' }}>Total Payable: <span style={{ color: 'red' }}>${total}</span></h3>
             <h3 style={{ color: 'aquamarine' }}><u>NameList and Images: </u></h3>
             {personCart.map(personinfo => <PersonName
