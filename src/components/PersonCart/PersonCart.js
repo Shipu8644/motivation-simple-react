86db7fa88573273662d4d54cfaaab2bed0179fae
Shipu8644 <shipu.monjurur@gmail.com>
2021-09-27 11:28:51 +0600

    delete fix left

--- src/components/PersonCart/PersonCart.js
+++ src/components/PersonCart/PersonCart.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import PersonName from '../PersonName/PersonName';
 import './PersonCart.css'
 const PersonCart = (props) => {
     const element = <FontAwesomeIcon icon={faUser} />
-    console.log(props);
+
     const { personCart } = props;
+    const [existingCart, setExistingCart] = useState([]);
+
+    useEffect(() => {
+        setExistingCart(personCart);
+    }, [personCart])
+
+    console.log(existingCart);
+    const closeHandler = (person) => {
+        const existCart = existingCart.filter(p => p.name !== person.name);
+        setExistingCart(existCart);
+    }
+    // console.log(existingCart);
+
     const total = personCart.reduce((a, b) => a + b.earning, 0);
+
+
 
     return (
         <div className="personCart">
-            <h2 ><i style={{ color: 'aquamarine' }}>{element} Persons Added: <span style={{ color: 'red' }}> {personCart.length}</span></i></h2>
-            <h3 style={{ color: 'aquamarine' }}>Total Payable: <span style={{ color: 'red' }}>${total}</span></h3>
+            <h2 ><i style={{ color: 'green' }}>{element} Persons Added: <span style={{ color: 'red' }}> {personCart.length}</span></i></h2>
 
-            <h3 style={{ color: 'aquamarine' }}><u>NameList and Images: </u></h3>
-            {personCart.map(personinfo => <PersonName
+            <h3 style={{ color: 'green' }}> <strong>Total Payable: </strong> <span style={{ color: 'red' }}>${total}</span></h3>
+
+            <h3 style={{ color: 'green' }}><u>NameList and Images: </u></h3>
+            {existingCart.map(personinfo => <PersonName
                 key={personinfo.name}
-                personinfo={personinfo}></PersonName>)}
+                personinfo={personinfo}
+                closeHandler={closeHandler}
+            ></PersonName>)}
         </div>
     );
 };
--- src/components/PersonName/PersonName.js
+++ src/components/PersonName/PersonName.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import './PersonName.css'
-const PersonName = (props) => {
-    console.log(props);
-    const { img, name } = props.personinfo;
-    return (
-        <div style={{ display: 'flex', marginTop: '10px', color: 'red' }}>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
+import './PersonName.css';
 
-            <h3> {name}</h3>
+const PersonName = (props) => {
+    // console.log(props);
+    const { img, name } = props.personinfo;
+    const element = <FontAwesomeIcon icon={faWindowClose} />
+    return (
+        <div className='person-name'>
+            <h1 onClick={() => props.closeHandler(props.personinfo)}
+                style={{ color: 'red', cursor: 'pointer' }}>{element}</h1>
+            <h3 style={{ marginLeft: '5px', marginRight: '5px' }}> {name}</h3>
             <img style={{ width: '200px', borderRadius: '50%' }} src={img} alt="" />
         </div>
     );
